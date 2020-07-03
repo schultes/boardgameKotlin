a95c69f60956b04b4618a2ba6a59bfd629e8a9d5
Dominik Schultes <dominik.schultes@iem.thm.de>
2020-07-03 16:22:55 +0200

    chess piece added

---
+++ app/src/main/java/de/thm/mow/boardgame/model/chess/ChessPiece.kt
@@ -0,0 +1,62 @@
+package de.thm.mow.boardgame.model.chess
+
+import de.thm.mow.boardgame.model.Player
+import de.thm.mow.boardgame.model.support.*
+
+enum class ChessPiece(val rawValue: String) {
+    Empty("  "),
+    Invalid("x"),
+    WhitePawn("♙"),
+    BlackPawn("♟"),
+    WhiteKnight("♘"),
+    BlackKnight("♞"),
+    WhiteBishop("♗"),
+    BlackBishop("♝"),
+    WhiteRook("♖"),
+    BlackRook("♜"),
+    WhiteQueen("♕"),
+    BlackQueen("♛"),
+    WhiteKing("♔"),
+    BlackKing("♚");
+    companion object {
+        fun getPawn(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhitePawn else BlackPawn
+        }
+
+        fun getKnight(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhiteKnight else BlackKnight
+        }
+
+        fun getBishop(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhiteBishop else BlackBishop
+        }
+
+        fun getRook(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhiteRook else BlackRook
+        }
+
+        fun getQueen(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhiteQueen else BlackQueen
+        }
+
+        fun getKing(@argLabel("forPlayer") player: Player) : ChessPiece {
+            return if (player == Player.white) WhiteKing else BlackKing
+        }
+    }
+
+    override fun toString(): String {
+        return rawValue
+    }
+
+    fun belongs(@argLabel("toPlayer") player: Player) : Boolean {
+        if (((this == WhitePawn) || (this == WhiteKing)) && (player == Player.white)) {
+            return true
+        }
+
+        if (((this == BlackPawn) || (this == BlackKing)) && (player == Player.black)) {
+            return true
+        }
+
+        return false
+    }
+}
